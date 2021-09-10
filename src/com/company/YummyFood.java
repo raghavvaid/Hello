869bf19bba19395dc4f8211cddb2d4d813589f10
Raghav <raghavvaid@billericak12.com>
2021-09-10 11:34:37 -0400

    owiajfowajfio

--- src/com/company/YummyFood.java
+++ src/com/company/YummyFood.java
@@ -1,4 +1,22 @@
 package com.company;
 
+
 public class YummyFood {
+    public static void main(String[] args) {
+        System.out.print("Yummy ");
+        System.out.println("Food");
+        chocolate();
+        chocolate();
+        iceCream();
+        System.out.println("done");
+    }
+    public static void chocolate(){
+        System.out.println("Chocolate");
+
+    }
+    public static void iceCream(){
+        System.out.println("Chunky Chocolate Pudding");
+        chocolate();
+        System.out.println("that was yummy");
+    }
 }
