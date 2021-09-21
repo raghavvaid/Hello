8524809a36a62aa989a2f6c3ec11ac7ec3dcf1a3
Raghav Vaid <raghavvaid@billericak12.com>
2021-09-21 18:52:20 -0400

    dfwfwef

--- src/com/company/MathExercises.java
+++ src/com/company/MathExercises.java
@@ -3,22 +3,28 @@ package com.company;
 public class MathExercises {
     public static int digitSwapper(int input) {
         int numOne = input % 10;
-        int numTen = input/10 % 10;
-        int z = input - (numOne+numTen*10);
-        return z+numOne*10+numTen;
-
+        int numTen = input / 10 % 10;
+        int z = input - (numOne + numTen * 10);
+        return z + numOne * 10 + numTen;
     }
 
-//    public static void timeLeft(int currHour, int currMin, int depHour, int depMin)
-//    {
-//        //PRINT the time left here in hours and minutes
-//    }
-//    public static int dayOfWeek(int daysOfWeek1, int day)
-//    {
-//
-//        // returns an integer corresponding to the day of the week
-//    }
 
+    public static void timeLeft(int currHour, int currMin, int depHour, int depMin) {
+        int hourToMin = currHour * 60;
+        int curMinTotal = hourToMin + currMin;
+        int depHourToMin = depHour * 60;
+        int depMinTotal = depHourToMin + depMin;
+        int waitTime = depMinTotal - curMinTotal;
+        int A = waitTime / 60;
+        int B = waitTime % 60;
+        System.out.println("The train is arriving in " + A + "hours and " + B + " minutes.");
+    }
+
+    public static int dayOfWeek(int daysOfWeek1, int day) {
+        int x = daysOfWeek1 + day - 1;
+        int y = x % 7;
+        return y;
+    }
 
 
     public static void main(String[] args) {
@@ -34,15 +40,15 @@ public class MathExercises {
         input = 381;
         output = digitSwapper(input);
         System.out.println(input + " " + output);
-    }
-//        timeLeft(1,34, 8, 20);
-//        timeLeft(1,15, 4, 36);
+        timeLeft(1, 34, 8, 20);
+        timeLeft(1, 15, 4, 36);
 
-//        System.out.println(dayOfWeek(0,1));
-//        System.out.println(dayOfWeek(0,14));
-//        System.out.println(dayOfWeek(6, 22));
-//        System.out.println(dayOfWeek(5, 4));
-//        System.out.println(dayOfWeek(1,24));
-//        System.out.println(dayOfWeek(2,1));
-//
-  }
+        System.out.println(dayOfWeek(0, 1));
+        System.out.println(dayOfWeek(0, 14));
+        System.out.println(dayOfWeek(6, 22));
+        System.out.println(dayOfWeek(5, 4));
+        System.out.println(dayOfWeek(1, 24));
+        System.out.println(dayOfWeek(2, 1));
+
+    }
+}
